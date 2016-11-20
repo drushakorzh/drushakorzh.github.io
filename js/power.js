fb1558e9cb42c1b5786f2b824261333b6fdc00eb
Andriy Korzh <drushakorzh@gmail.com>
2016-11-21 01:08:57 +0200

    some major style and content changes
    -added new STORE page
    -added section subheader, which changes to menu subheader, when menu is
    activated
    -changed static hamburger menu icon to animated
    -other minor UI and code fixes

--- js/power.js
+++ js/power.js
@@ -1,7 +1,8 @@
 $(function() {
   $('.toggle-nav').click(function(event) {
 	  //event.stopPropagation();
-    if ($(".wrapper").css('opacity') == 0) $('.wrapper').css('opacity', 1);
-    else $('.wrapper').css('opacity', 0);
+    if ($('.wrapper').css('opacity') == 0) $('.wrapper').css('opacity', 1), $('.uniqhdr').css('opacity', 0);
+    else $('.wrapper').css('opacity', 0), $('.uniqhdr').css('opacity', 1);;
+    $('.burger').toggleClass('open');
   });
 });
